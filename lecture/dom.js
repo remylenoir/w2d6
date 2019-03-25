a01acf19dfaf30e71813f85a0a6ac921356c341d
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-03-25 15:00:19 +0100

    DOM lecture

--- lecture/dom.js
+++ lecture/dom.js
@@ -1,60 +1,81 @@
-// Get an element by the Tag Name
-const heading = document.getElementsByTagName("h1");
-const button = document.getElementsByTagName("button");
-const list = document.querySelectorAll(".groceries li");
+const headings = document.getElementsByTagName("h1");
+const myHeading = Array.from(headings)[0];
 
-// To access to the first heading
-const myHeading = Array.from(heading)[0];
-const myButton = Array.from(button)[0];
+const introDiv = document.querySelector(".intro");
 
-// To list all the attributes / values (or properties) in the console
-// console.dir(myHeading);
+const buttons = document.getElementsByTagName("button");
+const myButton = buttons[0];
 
-// Change the content of the element
-myHeading.innerHTML = "This heading has been updated with JavaScript.";
+console.dir(myButton);
 
-// Execute a function that will run 'n' amount of time
+// setInterval(function() {
+//     if (myButton.style.backgroundColor === 'yellow') {
+//         myButton.style.backgroundColor = 'red'
+//     } else {
+//         myButton.style.backgroundColor = 'yellow'
+//     }
+// }, 100)
+
+// setTimeout(function() {
+//     // const myParagraph = document.createElement('p')
+//     // myParagraph.innerHTML = 'Hello there, please accept our cookie policy'
+//     // myParagraph.classList.add('paragraph-primary')
+//     // document.body.appendChild(myParagraph)
+
+//     const myImg = document.createElement('img')
+//     myImg.style.width = '500px'
+//     myImg.src =
+//         'https://images.unsplash.com/photo-1553424713-55a8239f28aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+//     document.querySelector('.intro').appendChild(myImg)
+// }, 1500)
+
+// display all groceries
+// const listItems = document.querySelectorAll('ul.groceries li')
+// listItems.forEach(function(listItem) {
+//     console.log(listItem.innerText)
+// })
+
 setInterval(function() {
-  if (myButton.style.backgroundColor === "red") {
-    myButton.style.backgroundColor = "#3b81d8";
-  } else {
-    myButton.style.backgroundColor = "red";
+  const nextItem = document.querySelector("li");
+  if (!nextItem) return;
+  // nextItem.classList.toggle('purchased')
+  // nextItem.classList.replace('not-purchased', 'purchased')
+  // console.log('nextItem :', nextItem.classList)
+  // classList.value.includes('purchased')
+  // classList.add()
+  // classList.remove()
+  // classList.toggle()
+  // classList.value
+}, 2000);
+
+// document.querySelector('button').addEventListener('click', evt => {
+//     const nextItem = document.querySelector('li.not-purchased')
+//     if (!nextItem) return
+//     nextItem.classList.replace('not-purchased', 'purchased')
+// })
+
+document.querySelectorAll(".groceries li").forEach(function(listItem) {
+  listItem.addEventListener("click", evt => {
+    evt.target.classList.toggle("purchased");
+  });
+});
+
+document.querySelector("button").addEventListener("click", evt => {
+  const newItemText = document.querySelector("#groceries-input").value;
+  if (
+    Array.from(document.querySelectorAll(".groceries li")).find(listItem => {
+      if (listItem.innerHTML.toLowerCase().trim() === newItemText.toLowerCase().trim()) return true;
+    })
+  ) {
+    return;
   }
-}, 500);
 
-// Execute a function that will run once: setTimeout()
-// Create a new element
-// Append it to the body or to another element
-// setTimeout(function() {
-//   const myParagraph = document.createElement("p");
-//   myParagraph.innerHTML =
-//     "Irure Lorem commodo dolor ipsum aliqua ullamco aliquip cillum occaecat ut. Voluptate culpa esse proident eiusmod occaecat consequat non. Minim eiusmod do dolore excepteur cillum anim voluptate sunt sint ex. Aliqua amet proident laborum velit aliquip laborum qui sunt. Ea ad laboris non enim enim dolore et ad ad. Irure voluptate deserunt deserunt velit pariatur aliqua ipsum.";
-//   document.body.appendChild(myParagraph);
-// }, 1500);
-
-// Create an image and put it on the page
-// setTimeout(function() {
-//   const myImage = document.createElement("img");
-//   myImage.style.height = "250px";
-//   myImage.src =
-//     "https://images.unsplash.com/photo-1553424713-55a8239f28aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80";
-//   // document.body.appendChild(myImage);
-//   document.querySelector(".intro").appendChild(myImage);
-// }, 500);
-
-// Function that logs all the elements of the Array
-list.forEach(item => console.log(item.innerText));
-
-setInterval(function() {
-  const nextItem = document.querySelector("li.not-purchased");
-  if (!nextItem) return; // break the function if no items are met anymore
-
-  nextItem.classList.replace("not-purchased", "purchased");
-}, 500);
-
-// classList.value.contains("purchased")
-// classList.add()
-// classList.remove()
-// classList.toggle()
-// classList.replace()
-// classList.value()
+  const newItem = document.createElement("li");
+  newItem.addEventListener("click", evt => {
+    evt.target.classList.toggle("purchased");
+  });
+  newItem.innerHTML = newItemText;
+  newItem.classList.add("not-purchased");
+  document.querySelector("ul.groceries").prepend(newItem);
+  document.querySelector("#groceries-input").value = "";
+});
